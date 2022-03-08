f8d599c66574fe05cc7be67e5c50e0e12b5293b0
keogami <41939011+keogami@users.noreply.github.com>
2022-03-08 15:17:30 +0530

    implemented GetBlogs method

--- api/getblogs.go
+++ api/getblogs.go
@@ -1,7 +1,48 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
+	"blog/db"
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
-func GetBlogs(c *gin.Context) {}
+type Meta struct {
+  Slug string `json:"slug"`
+  Title string `json:"title"`
+  Group interface{} `json:"group"`
+  PostTime time.Time `json:"postTime"`
+  Summary string `json:"summary"`
+  Tags []string `json:"tags"`
+}
+
+func MetaFrom(m db.Meta) Meta {
+  return Meta{
+    Slug: m.Slug,
+    Title: m.Title,
+    Group: JsonNullString(m.GroupName),
+    PostTime: m.PostTime,
+    Summary: m.Summary,
+    Tags: m.Tags,
+  }
+}
+
+func GetBlogs(c *gin.Context) {
+  ctx := context.Background()
+  q := GetDBQueries()
+
+  dbmetas, err := q.ListMetas(ctx)
+  if err != nil {
+    c.JSON(http.StatusInternalServerError, gin.H{ "message": err.Error() })
+    return
+  }
+
+  metas := make([]Meta, 0, len(dbmetas))
+  for _, item := range dbmetas {
+    metas = append(metas, MetaFrom(item))
+  }
+
+  c.JSON(http.StatusOK, metas)
+}
