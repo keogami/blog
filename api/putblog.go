90a947f371037030dccbff9ffb8a6435a6ae08fe
keogami <41939011+keogami@users.noreply.github.com>
2022-03-10 11:41:51 +0530

    implemented PutBlog method

---
+++ api/putblog.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"blog/db"
+	"context"
+	"database/sql"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func PutBlog(c *gin.Context) {
+  var post Post
+  if err := c.ShouldBindJSON(&post); err != nil {
+    c.JSON(http.StatusBadRequest, gin.H{ "message": err.Error() })
+    return
+  }
+  slug := c.Param("slug")
+
+  ctx := context.Background()
+  q := GetDBQueries()
+
+  id, err := q.UpdatePost(ctx, db.UpdatePostParams{
+    Slug: slug,
+    Content: post.Content,
+  })
+  if err != nil {
+    c.JSON(http.StatusInternalServerError, gin.H{ "message": err.Error() })
+    return
+  }
+
+  err = q.UpdateMeta(ctx, db.UpdateMetaParams{
+    PostID: id,
+    Slug: post.Meta.Slug,
+    Title: post.Meta.Title,
+    Summary: post.Meta.Summary,
+    GroupName: sql.NullString{ String: post.Meta.Group.(string), Valid: true },
+    Tags: post.Meta.Tags,
+  })
+  if err != nil {
+    c.JSON(http.StatusInternalServerError, gin.H{ "message": err.Error() })
+    return
+  }
+
+  c.JSON(http.StatusOK, post) // TODO stop being lazy
+}
--- api/router.go
+++ api/router.go
@@ -9,4 +9,5 @@ func BindRoutes(r *gin.Engine) {
   r.GET("/blog/:slug", GetBlogBySlug)
 
   r.POST("/blog", PostBlog)
+  r.PUT("/blog/:slug", PutBlog)
 }
